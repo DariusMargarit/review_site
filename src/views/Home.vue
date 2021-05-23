d370a739ea658ce6e2cd3ce3f0424c858eea3c42
3 <77794481+smallpixel1105@users.noreply.github.com>
2021-05-23 21:44:51 +0300

    baza renovarii

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,132 +1,228 @@
 <template>
-  <v-main >
-
+  <v-main>
     <nav-bar />
 
-    <div class="first_content">
-      <br /><br /><br />
+<!--    <div class="first_content">-->
+<!--      <br /><br /><br />-->
 
-      <div align="center" class="mt-3">
-          <a href="#ghid" class="ghidul">
-            <span class="ma-4 font-weight-bold" style="color: black;">Ghidul utilizatorului</span>
-          </a>
-      </div>
+<!--      <div align="center" class="mt-3">-->
+<!--        <a href="#ghid" class="ghidul">-->
+<!--          <span class="ma-4 font-weight-bold" style="color: black;">Ghidul utilizatorului</span>-->
+<!--        </a>-->
+<!--      </div>-->
 
-      <div class="content">
-        <div class="content-col content-text">
-          <p>Doresti si tu sa lasi un review unui produs? Creeaza-ti un cont chiar acum</p>
-          <br />
-          <v-btn v-if="userIsAuthenticated" disabled large class="homebutton text--black font-weight-bold" plain >Înscrie-te</v-btn>
-          <v-btn v-else large class="homebutton text--black font-weight-bold" plain @click="toSignUp" >Înscrie-te</v-btn>
-        </div>
-        <div class="content-col mr-15">
-          <img src="../assets/rating.png" class="img" />
-        </div>
-      </div>
-      <div class="subcontent">
-        <div class="content-col subcontent-text">
-          <p>Ai deja un cont? Conecteaza-te la el aici</p>
+<!--      <div class="content">-->
+<!--        <div class="content-col content-text">-->
+<!--          <p>Doresti si tu sa lasi un review unui produs? Creeaza-ti un cont chiar acum</p>-->
+<!--          <br />-->
+<!--          <v-btn v-if="userIsAuthenticated" disabled large class="homebutton text&#45;&#45;black font-weight-bold" plain >Înscrie-te</v-btn>-->
+<!--          <v-btn v-else large class="homebutton text&#45;&#45;black font-weight-bold" plain @click="toSignUp" >Înscrie-te</v-btn>-->
+<!--        </div>-->
+<!--        <div class="content-col mr-15">-->
+<!--          <img src="../assets/rating.png" class="img" />-->
+<!--        </div>-->
+<!--      </div>-->
+<!--      <div class="subcontent">-->
+<!--        <div class="content-col subcontent-text">-->
+<!--          <p>Ai deja un cont? Conecteaza-te la el aici</p>-->
 
-          <br />
-          <v-btn v-if="userIsAuthenticated" disabled large class="homebutton text--black font-weight-bold" plain >Autentifică-te</v-btn>
-          <v-btn v-else large class="homebutton text--black font-weight-bold" plain @click="toLogIn" >Autentifică-te</v-btn>
-        </div>
-      </div>
+<!--          <br />-->
+<!--          <v-btn v-if="userIsAuthenticated" disabled large class="homebutton text&#45;&#45;black font-weight-bold" plain >Autentifică-te</v-btn>-->
+<!--          <v-btn v-else large class="homebutton text&#45;&#45;black font-weight-bold" plain @click="toLogIn" >Autentifică-te</v-btn>-->
+<!--        </div>-->
+<!--      </div>-->
 
 
 
-    </div>
+<!--    </div>-->
+
+<!--    <div style="padding:20px" class="separator"><v-icon>mdi-chevron-triple-left</v-icon></div>-->
+<!--    <br />-->
+
+
+
+<!--    <div class="content-part">-->
+<!--      <div class="content-box">-->
+<!--        <div class="content-title">-->
+<!--          <h1>Ce este HonestReviews?</h1>-->
+<!--        </div>-->
+
+<!--        <div class="content-parag">-->
+<!--          <p style="font-family: 'Lato', sans-serif; font-weight: bold;">-->
+<!--            HonestReviews este un website prin intermediul caruia utilizatorii pot lasa review-uri, adica pareri, oricarui produs pe care l-au achizitionat.-->
+<!--            Un asa-zis "review" consta in exprimarea propriei opinii in legatura cu produsul ales.-->
+<!--            De asemenea, sunt mentionate sursa de unde a fost achizitionat produsul, numele intreg al acestuia, pretul si alte informatii utile.-->
+<!--          </p>-->
+<!--        </div>-->
+<!--      </div>-->
+
+<!--      <div class="content-image">-->
+<!--        <img src="../assets/social-media-3846597_640.png" />-->
+<!--      </div>-->
+
+<!--    </div>-->
+
+
+<!--    <br />-->
+<!--    <div style="padding:20px" class="separator"><v-icon>mdi-chevron-triple-right</v-icon></div>-->
+<!--    <br />-->
+
+<!--    <div class="content-part">-->
+
+<!--      <div class="content-image">-->
+<!--        <img style="background: white; max-width: 950px" src="../assets/social-media-5187243_1280.png"  />-->
+<!--      </div>-->
+
+<!--      <div class="content-box mr-15">-->
+
+
+<!--        <div class="content-title">-->
+<!--          <h1>De ce HonestReviews?</h1>-->
+<!--        </div>-->
+
+<!--        <div class="content-parag">-->
+<!--          <p style="font-family: 'Lato', sans-serif; font-weight: bold;">-->
+<!--            Site-ul nostru va ofera o modalitate usoara de a scrie sau de a cauta impresii ale altor utilizatori despre orice fel ce produs.-->
+<!--            De la frigidere la computere, de la masini la biciclete, de la resorturi turistice la restaurante, aici puteti gasi un review pentru orice categorie imaginabila.-->
+<!--            Datorita interfatei construita sa fie usor de folosit, oricine poate beneficia de acest site.-->
+<!--            In plus, autentificarea se poate realiza prin intermediul E-Mailului, al contului de Google sau al contului de Facebook, asigurand, astfel, un proces rapid de logare.-->
+<!--          </p>-->
+<!--        </div>-->
+<!--      </div>-->
+
+
+
+<!--    </div>-->
+
+<!--    <div style="padding:20px" class="separator"><v-icon>mdi-chevron-triple-left</v-icon></div>-->
+<!--    <br />-->
+
+
+
+<!--    <div class="content-part">-->
+<!--      <div class="content-box">-->
+<!--        <div class="content-title">-->
+<!--          <h1 id="ghid">Ghidul utilizatorului</h1>-->
+<!--        </div>-->
+
+<!--        <div class="content-parag">-->
+<!--          <p style="font-family: 'Lato', sans-serif; font-weight: bold;">-->
+<!--            Este usor sa folosesti HonestReviews. Daca nu ai un cont sau nu esti autentificat, vei putea doar sa vezi produsele si review-urile, nu si sa adaugi altele.-->
+<!--            Altfel, daca nu ai un cont, il poti creea apasand butonul "Inscrie-te", pe pagina de acasa sau un partea de sus a ecranului.-->
+<!--            Daca ai deja un cont, te poti autentifica, apasand butonul "Autentifica-te".-->
+<!--            Mai departe, pentru a adauga un produs, trebuie sa apesi pe butonul "Categorii" si sa selectezi categoria in care se incadreaza acesta.-->
+<!--            Pentru a-l adauga, trebuie doar sa completezi formularul ("Adauga un produs").-->
+<!--            Dupa ce produsul a fost agaugat, in cazul in care nu exista deja, facand click pe el, ajungi la pagina unde vezi review-urile existente, si unde poti accesa formularul pentru review.-->
+<!--          </p>-->
+<!--          <br />-->
+<!--          <v-btn @click="goToCat" large class="homebutton text&#45;&#45;black font-weight-bold" plain>-->
+<!--            Categorii-->
+<!--          </v-btn>-->
+<!--        </div>-->
+<!--      </div>-->
+
+<!--      <div class="content-image">-->
+<!--        <img src="../assets/calculator.jpg" />-->
+<!--      </div>-->
+
+<!--    </div>-->
+    <v-content class="first_content">
+
+      <v-container fluid>
+        <a href="#ghid" class="ghidul">
+          <span class="ma-4 font-weight-bold" style="color: black;">Ghidul utilizatorului</span>
+        </a>
+      </v-container>
+
+      <v-container class="home-container">
+        <v-layout>
+          <v-flex>
+            <p class="content-text">Doresti si tu sa lasi un review unui produs? Creeaza-ti un cont chiar acum</p>
+            <br />
+            <v-btn v-if="userIsAuthenticated" disabled large class="homebutton text--black font-weight-bold" plain >Înscrie-te</v-btn>
+            <v-btn v-else large class="homebutton text--black font-weight-bold" plain @click="toSignUp" >Înscrie-te</v-btn>
+          </v-flex>
+        </v-layout>
+        <v-layout>
+          <v-flex style="margin-left: 50%; margin-right: 0; padding-right: 0;">
+            <img class="img" src="../assets/rating.png" />
+          </v-flex>
+        </v-layout>
+      </v-container>
+
+      <v-container class="second-home-container">
+        <v-layout>
+          <v-flex>
+            <p class="content-text">Ai deja un cont? Conecteaza-te la el aici</p>
+            <br />
+            <v-btn v-if="userIsAuthenticated" disabled large class="homebutton text--black font-weight-bold" plain >Autentifică-te</v-btn>
+            <v-btn v-else large class="homebutton text--black font-weight-bold" plain @click="toLogIn" >Autentifică-te</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
 
     <div style="padding:20px" class="separator"><v-icon>mdi-chevron-triple-left</v-icon></div>
-    <br />
 
+    <v-container style="display: flex; align-content: center; justify-content: center;">
+        <v-layout style="justify-content: center; display: flex;">
+          <v-flex>
+              <h1>Ce este HonestReviews?</h1>
+              <p style="font-family: 'Lato', sans-serif; font-weight: bold;">
+                HonestReviews este un website prin intermediul caruia utilizatorii pot lasa review-uri, adica pareri, oricarui produs pe care l-au achizitionat.
+                Un asa-zis "review" consta in exprimarea propriei opinii in legatura cu produsul ales.
+                De asemenea, sunt mentionate sursa de unde a fost achizitionat produsul, numele intreg al acestuia, pretul si alte informatii utile.
+              </p>
+            </v-flex>
+          <v-flex style="display: flex; align-content: center; justify-content: center; margin-left: 20%;">
+            <img src="../assets/social-media-3846597_640.png" />
+          </v-flex>
+        </v-layout>
+    </v-container>
 
-
-    <div class="content-part">
-      <div class="content-box">
-        <div class="content-title">
-          <h1>Ce este HonestReviews?</h1>
-        </div>
-
-        <div class="content-parag">
-          <p style="font-family: 'Lato', sans-serif; font-weight: bold;">
-            HonestReviews este un website prin intermediul caruia utilizatorii pot lasa review-uri, adica pareri, oricarui produs pe care l-au achizitionat.
-            Un asa-zis "review" consta in exprimarea propriei opinii in legatura cu produsul ales.
-            De asemenea, sunt mentionate sursa de unde a fost achizitionat produsul, numele intreg al acestuia, pretul si alte informatii utile.
-          </p>
-        </div>
-      </div>
-
-      <div class="content-image">
-        <img src="../assets/social-media-3846597_640.png" />
-      </div>
-
-    </div>
-
-
-    <br />
     <div style="padding:20px" class="separator"><v-icon>mdi-chevron-triple-right</v-icon></div>
-    <br />
 
-    <div class="content-part">
-
-      <div class="content-image">
-        <img style="background: white; max-width: 950px" src="../assets/social-media-5187243_1280.png"  />
-      </div>
-
-      <div class="content-box mr-15">
-
-
-        <div class="content-title">
+    <v-container style="display: flex; align-content: center; justify-content: center;">
+      <v-layout style="justify-content: center; display: flex;">
+        <v-flex>
           <h1>De ce HonestReviews?</h1>
-        </div>
-
-        <div class="content-parag">
           <p style="font-family: 'Lato', sans-serif; font-weight: bold;">
             Site-ul nostru va ofera o modalitate usoara de a scrie sau de a cauta impresii ale altor utilizatori despre orice fel ce produs.
             De la frigidere la computere, de la masini la biciclete, de la resorturi turistice la restaurante, aici puteti gasi un review pentru orice categorie imaginabila.
             Datorita interfatei construita sa fie usor de folosit, oricine poate beneficia de acest site.
             In plus, autentificarea se poate realiza prin intermediul E-Mailului, al contului de Google sau al contului de Facebook, asigurand, astfel, un proces rapid de logare.
           </p>
-        </div>
-      </div>
-
-
-
-    </div>
+        </v-flex>
+        <v-flex style="display: flex; align-content: center; justify-content: center; margin-left: 20%;">
+          <img style="width:100%; height: 100%;" src="../assets/social-media-5187243_1280.png" />
+        </v-flex>
+      </v-layout>
+    </v-container>
 
     <div style="padding:20px" class="separator"><v-icon>mdi-chevron-triple-left</v-icon></div>
-    <br />
 
-
-
-    <div class="content-part">
-      <div class="content-box">
-        <div class="content-title">
-          <h1 id="ghid">Ghidul utilizatorului</h1>
-        </div>
-
-        <div class="content-parag">
+    <v-container style="display: flex; align-content: center; justify-content: center;">
+      <v-layout style="justify-content: center; display: flex;">
+        <v-flex>
+          <h1>Ghidul utilizatorului</h1>
           <p style="font-family: 'Lato', sans-serif; font-weight: bold;">
-              Este usor sa folosesti HonestReviews. Daca nu ai un cont sau nu esti autentificat, vei putea doar sa vezi produsele si review-urile, nu si sa adaugi altele.
-              Altfel, daca nu ai un cont, il poti creea apasand butonul "Inscrie-te", pe pagina de acasa sau un partea de sus a ecranului.
-              Daca ai deja un cont, te poti autentifica, apasand butonul "Autentifica-te".
-              Mai departe, pentru a adauga un produs, trebuie sa apesi pe butonul "Categorii" si sa selectezi categoria in care se incadreaza acesta.
-              Pentru a-l adauga, trebuie doar sa completezi formularul ("Adauga un produs").
-              Dupa ce produsul a fost agaugat, in cazul in care nu exista deja, facand click pe el, ajungi la pagina unde vezi review-urile existente, si unde poti accesa formularul pentru review.
+            Este usor sa folosesti HonestReviews. Daca nu ai un cont sau nu esti autentificat, vei putea doar sa vezi produsele si review-urile, nu si sa adaugi altele.
+            Altfel, daca nu ai un cont, il poti creea apasand butonul "Inscrie-te", pe pagina de acasa sau un partea de sus a ecranului.
+            Daca ai deja un cont, te poti autentifica, apasand butonul "Autentifica-te".
+            Mai departe, pentru a adauga un produs, trebuie sa apesi pe butonul "Categorii" si sa selectezi categoria in care se incadreaza acesta.
+            Pentru a-l adauga, trebuie doar sa completezi formularul ("Adauga un produs").
+            Dupa ce produsul a fost agaugat, in cazul in care nu exista deja, facand click pe el, ajungi la pagina unde vezi review-urile existente, si unde poti accesa formularul pentru review.
           </p>
           <br />
           <v-btn @click="goToCat" large class="homebutton text--black font-weight-bold" plain>
             Categorii
           </v-btn>
-        </div>
-      </div>
-
-      <div class="content-image">
-        <img src="../assets/calculator.jpg" />
-      </div>
-
-    </div>
+        </v-flex>
+        <v-flex style="display: flex; align-content: center; justify-content: center; margin-left: 20%;">
+          <img src="../assets/calculator.jpg" />
+        </v-flex>
+      </v-layout>
+    </v-container>
 
   </v-main>
 </template>
@@ -135,8 +231,6 @@
 import Signup from "./Signup";
 import Login from "./Login";
 import Navbar from "../components/Navbar";
-
-
 export default {
   components: {
     'sign-up': Signup,
@@ -163,16 +257,33 @@ export default {
 </script>
 
 <style>
-
 .homebutton{
   background-color: hsl(47, 95%, 49%);
 }
-
 .first_content{
   background-image: linear-gradient(180deg, rgba(0,204,255,0.05) 0%, rgba(0,204,255,0.1) 100%),
   url("../assets/tori-1976609_1920.jpg");
   background-position: center;
   background-size: cover;
+  margin-top: 4%;
+}
+
+.home-container {
+  width: 100%;
+  align-content: center;
+  justify-content: center;
+  display: flex;
+  text-align: center;
+  margin-left: 5%;
+}
+
+.second-home-container {
+  width: 30%;
+  align-content: center;
+  justify-content: center;
+  display: flex;
+  text-align: center;
+  margin-left: 5%;
 }
 .content-text{
   font-size: 34px;
@@ -229,11 +340,12 @@ export default {
   align-items: center;
   padding: 40px;
   margin: 30px;
-
 }
 .img {
   width: 100%;
   height: 100%;
+  padding: 0;
+  margin: 0;
 }
 .content-box{
   width: 40%;
@@ -255,7 +367,6 @@ export default {
   margin-right: 30px;
   padding-right: 20px;
 }
-
 .ghidul {
   text-align: center;
   padding: 10px;
