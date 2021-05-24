d97f9009a19fcf5c3354dbdc1d75ff9008024717
DariusMargarit <margaritdariu89@yahoo.com>
2021-05-24 20:47:21 +0300

    filtre adaugate

--- src/views/categorie.vue
+++ src/views/categorie.vue
@@ -14,24 +14,16 @@
                     <add-product :catKey="catKey" />
                 </div>
                 <div class="zonaFiltre">
-                    <div class="Filtre" style="padding:15px; font-family: 'Lato', sans-serif; font-weight: bold;">
-                        <H3 style="margin-bottom:8px">Cauta dupa stele</H3>
-                        <div style="margin-top: 0px">
-                            <v-checkbox  class="stele" v-model="stea" value="1" :label="`1 stea`"></v-checkbox>
-                            <v-checkbox  class="stele" v-model="stea" value="2" :label="`2 stele`"></v-checkbox>
-                            <v-checkbox  class="stele" v-model="stea" value="3" :label="`3 stele`"></v-checkbox>
-                            <v-checkbox  class="stele" v-model="stea" value="4" :label="`4 stele`"></v-checkbox>
-                            <v-checkbox  class="stele" v-model="stea" value="5" :label="`5 stele`"></v-checkbox>
-                            <v-checkbox  class="stele" :label="`Selecteaza tot`"></v-checkbox>
+                    <div class="Filtre" style="padding:15px;">
+                        <H3 style="margin-bottom:8px">Cauta dupa nr. de stele</H3>
+                        <div style="margin-top: 0px" v-for="(filtru, index) in stele">
+                            <v-checkbox  class="stele" v-model="selected.stele" :value="index + 1" :label="filtru"></v-checkbox>
                         </div>
                     </div>
                     <div class="Filtre" style="padding:15px">
-                        <H3 style="margin-bottom:8px; padding:15px; font-family: 'Lato', sans-serif; font-weight: bold;">Nr. de review-uri</H3>
-                        <div style="margin-top: 0px">
-                            <v-checkbox  class="stele" v-model="nr" value="5" :label="`0-5`"></v-checkbox>
-                            <v-checkbox  class="stele" v-model="nr" value="10" :label="`5-10`"></v-checkbox>
-                            <v-checkbox  class="stele" v-model="nr" value="10000" :label="`10+`"></v-checkbox>
-                            <v-checkbox  class="stele" :label="`Selecteaza tot`"></v-checkbox>
+                        <H3 style="margin-bottom:8px;">Cauta dupa nr. de review-uri</H3>
+                        <div style="margin-top: 0px" v-for="(filtru, index) in nrrev">
+                            <v-checkbox class="stele" v-model="selected.nrrev" :value="index + 1" :label="filtru"></v-checkbox>
                         </div>
                     </div>
                 </div>
@@ -80,20 +72,34 @@
             'add-product': AddProdusPopup
         },
         created () {
-            this.$store.dispatch('loadProduse', this.id)
+            this.$store.dispatch('loadProduse', {
+                id: this.id,
+                stele: [],
+                nrrev: []
+            })
             this.$store.dispatch('loadCatName', this.id)
         },
         data () {
             return {
-                stea:[],
-                nr:[],
-                allstea:false,
-                allnr:false,
                 catKey: this.id,
-                // ratmin:0,
-                // ratmax:5,
-                // revmin:0,
-                // revmax:10000
+                stele: ["1 stea", "2 stele", "3 stele", "4 stele", "5 stele"],
+                nrrev: ["1-5", "5-10", "10+"],
+                selected: {
+                    stele: [],
+                    nrrev: []
+                }
+            }
+        },
+        watch: {
+            selected: {
+                handler: function () {
+                    this.$store.dispatch('loadProduse', {
+                        id: this.id,
+                        stele: this.selected.stele,
+                        nrrev: this.selected.nrrev
+                    })
+                },
+                deep: true
             }
         },
         computed: {
@@ -103,30 +109,6 @@
             loading () {
                 return this.$store.getters.loading
             },
-            // allStea: {
-            //     set: function () {
-            //         this.allstea=!this.allstea;
-            //         if(this.allstea){
-            //             var nr = ["1","2","3","4","5"];
-            //             this.stea=nr;
-            //         }
-            //         else{
-            //             this.stea=[];
-            //         }
-            //     }
-            // },
-            // allNr: {
-            //     set: function () {
-            //         this.allnr=!this.allnr;
-            //         if(this.allnr){
-            //             var nr = ["5","10","10000"];
-            //             this.nr=nr;
-            //         }
-            //         else{
-            //             this.nr=[];
-            //         }
-            //     }
-            // },
             userIsAuthenticated () {
                 return this.$store.getters.user !== null && this.$store.getters.user !== undefined
             },
@@ -138,33 +120,6 @@
             goToProduct (id) {
                 this.$router.push('/categorii/' + this.id + '/produs/' + id)
             },
-            // filteredProd (value) {
-            //     let tempProd = value
-            //
-            //     tempProd = tempProd.filter((produs) => {
-            //         return (produs.rating <= this.ratmax && produs.rating >= this.ratmin)
-            //     })
-            //
-            //     return tempProd;
-            // },
-            // updateFilter(){
-            //     var mins=10,maxs=0,mine=100000,maxe=0;
-            //     this.stea.sort(function(a, b){return a - b});
-            //     this.nr.sort(function(a, b){return a - b});
-            //     for(var i=0;i<this.stea.length;i++){
-            //         if(this.stea[i]>maxs)maxs=this.stea[i];
-            //         if(this.stea[i]<mins)mins=this.stea[i];
-            //     }
-            //     for(var i=0;i<this.nr.length;i++){
-            //         if(this.nr[i]>maxe)maxe=this.nr[i];
-            //         if(this.nr[i]<mine&&this.nr[i]<1000){mine=this.nr[i]}
-            //         else if(this.nr[i]<mine)mine=15;
-            //     }
-            //     this.ratmin=mins-1;
-            //     this.ratmax=maxs;
-            //     this.revmin=mine-5;
-            //     this.revmax=maxe;
-            // },
         }
     }
 </script>
