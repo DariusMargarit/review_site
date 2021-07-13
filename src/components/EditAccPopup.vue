69f21701d70e3c78b72caed2a5d4eaa208af8c78
3 <77794481+smallpixel1105@users.noreply.github.com>
2021-07-14 01:25:29 +0300

    validare pentru nume

--- src/components/EditAccPopup.vue
+++ src/components/EditAccPopup.vue
@@ -77,12 +77,12 @@
 <script>
 import { validationMixin } from 'vuelidate'
 import { required, maxLength, minLength } from 'vuelidate/lib/validators'
-import alphaNum from "vuelidate/lib/validators/alphaNum";
+import alpha from "vuelidate/lib/validators/alpha";
 
 export default {
   mixins: [validationMixin],
   validations: {
-    newName: { required, minLength: minLength(3), alphaNum },
+    newName: { required, minLength: minLength(3), alpha: alpha },
     newBio: {required, maxLength: maxLength(100), minLength: minLength(3)},
   },
   props: ['userDet'],
@@ -172,7 +172,7 @@ export default {
       if(!this.$v.newName) return errors
       !this.$v.newName.required && errors.push('Numele este obligatoriu')
       !this.$v.newName.minLength && errors.push('Numele trebuie sa contina cel putin 3 litere')
-      !this.$v.newName.alphaNum && errors.push('Numele trebuie sa contina doar litere/cifre')
+      !this.$v.newName.alpha && errors.push('Numele trebuie sa contina doar litere')
       return errors
     },
     bioErrors () {
