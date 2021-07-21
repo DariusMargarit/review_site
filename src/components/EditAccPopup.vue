1847d8ebaf202d2eae3068019ab819c0f1d92db9
3 <77794481+smallpixel1105@users.noreply.github.com>
2021-07-21 15:49:46 +0300

    vuelidate la edit acc

--- src/components/EditAccPopup.vue
+++ src/components/EditAccPopup.vue
@@ -77,20 +77,23 @@
 <script>
 import { validationMixin } from 'vuelidate'
 import { required, maxLength, minLength } from 'vuelidate/lib/validators'
-// import alpha from "vuelidate/lib/validators/alpha";
+import alpha from "vuelidate/lib/validators/alpha";
 
-const speciale = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+// const speciale = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '( ͡° ͜ʖ ͡°)']
 
-// const caractere = (value) => value.indexOf(forEach(speciale)) <= 0
+// const caractere = (value) => value.indexOf('!') <= 0
 
-const caractere = (value) => {
-  Array.from(speciale).forEach(element => value.indexOf(element) <= 0);
-}
+
+
+// const caractere = (value) => {
+//   Array.from(speciale).forEach(element => value.indexOf(element) <= 0);
+//   return value
+// }
 
 export default {
   mixins: [validationMixin],
   validations: {
-    newName: { required, minLength: minLength(3), caractere},
+    newName: { required, minLength: minLength(3), alpha: alpha},
     newBio: {required, maxLength: maxLength(100), minLength: minLength(3)},
   },
   props: ['userDet'],
@@ -181,7 +184,7 @@ export default {
       if(!this.$v.newName) return errors
       !this.$v.newName.required && errors.push('Numele este obligatoriu')
       !this.$v.newName.minLength && errors.push('Numele trebuie sa contina cel putin 3 litere')
-      !this.$v.newName.caractere && errors.push('Numele trebuie sa contina doar litere/cifre')
+      !this.$v.newName.alpha && errors.push('Numele trebuie sa contina doar litere/cifre')
       return errors
     },
     bioErrors () {
