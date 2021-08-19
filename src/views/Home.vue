24282593078488a44215868a4f75b09737a38d55
DariusMargarit <margaritdariu89@yahoo.com>
2021-08-19 15:08:15 +0300

    prima incercare de route cu hash dinamic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -143,12 +143,15 @@
 <script>
 import Signup from "./Signup";
 import Login from "./Login";
-import Navbar from "../components/Navbar";
+import debounce from 'lodash.debounce'
+
 export default {
   components: {
     'sign-up': Signup,
-    'log-in': Login,
-    // 'nav-bar': Navbar
+    'log-in': Login
+  },
+  mounted () {
+    window.addEventListener('scroll', this.onScroll)
   },
   created() {
     window.scrollTo(0,0)
@@ -162,12 +165,67 @@ export default {
     },
     goToCat () {
       this.$router.push('/categorii/')
+    },
+    onScroll: debounce(function () {
+      this.setActiveHash()
+    }, 300),
+
+    setActiveHash () {
+      const sidebarLinks = [].slice.call(document.querySelectorAll(AHL_SIDEBAR_LINK_SELECTOR))
+      const anchors = [].slice.call(document.querySelectorAll(AHL_HEADER_ANCHOR_SELECTOR))
+              .filter(anchor => sidebarLinks.some(sidebarLink => sidebarLink.hash === anchor.hash))
+
+      const scrollTop = Math.max(
+              window.pageYOffset,
+              document.documentElement.scrollTop,
+              document.body.scrollTop
+      )
+
+      const scrollHeight = Math.max(
+              document.documentElement.scrollHeight,
+              document.body.scrollHeight
+      )
+
+      const bottomY = window.innerHeight + scrollTop
+
+      for (let i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i]
+        const nextAnchor = anchors[i + 1]
+
+        const isActive = i === 0 && scrollTop === 0
+                || (scrollTop >= anchor.parentElement.offsetTop + 10
+                        && (!nextAnchor || scrollTop < nextAnchor.parentElement.offsetTop - 10))
+
+        const routeHash = decodeURIComponent(this.$route.hash)
+        if (isActive && routeHash !== decodeURIComponent(anchor.hash)) {
+          const activeAnchor = anchor
+          // check if anchor is at the bottom of the page to keep $route.hash consistent
+          if (bottomY === scrollHeight) {
+            for (let j = i + 1; j < anchors.length; j++) {
+              if (routeHash === decodeURIComponent(anchors[j].hash)) {
+                return
+              }
+            }
+          }
+          this.$vuepress.$set('disableScrollBehavior', true)
+          this.$router.replace(decodeURIComponent(activeAnchor.hash), () => {
+            // execute after scrollBehavior handler.
+            this.$nextTick(() => {
+              this.$vuepress.$set('disableScrollBehavior', false)
+            })
+          })
+          return
+        }
+      }
     }
   },
   computed: {
     userIsAuthenticated () {
       return this.$store.getters.user !== null && this.$store.getters.user !== undefined
     }
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.onScroll)
   }
 }
 </script>
